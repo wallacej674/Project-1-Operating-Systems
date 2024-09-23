89c566a30fc4ac628a525c38fe9d3c9b8320350f
Jonathan <119520712+wallacej674@users.noreply.github.com>
2024-09-23 18:28:17 -0400

    This is the initial file
    This file contains nothing much but the libraries I think we will need throughout this project.

---
+++ Project1-Operating-Systems.c
@@ -0,0 +1,9 @@
+//Project 1 Jonathan Wallace, Christopher Bernal, third person anonymous.
+//This project is for the class Operating systems and is composed of 5 modules.
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+
